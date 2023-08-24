9b897428e68e887534719a19ce141fc35c0c5595
PureVodka007 <malarsurya.sp@gmail.com>
2023-08-24 21:00:29 +0530

    Add Week3 program
    added remove duplicates from array

--- Lab/OOPS Java/Week3/rmDup.java
+++ Lab/OOPS Java/Week3/rmDup.java
@@ -2,36 +2,79 @@ import java.util.Scanner;
 
 public class rmDup
 {
-    public Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
+    static int n;
 
-    static int isdup(int[] a, int n)
+    public static int[] mFill(int n)
+    {
+    
+        int[] a = new int[n];
+
+        System.out.println("Elements in matrix: ");
+
+        for(int i = 0; i<n;i++)
+        {
+            System.out.print("["+i+"]: ");
+            a[i] = sc.nextInt();
+        }
+        return a;
+    }
+
+    public static int[] DupR(int[] a, int n)
     {
         int[] dup = new int[n]; // creating a duplicate array
-
-        for( int i:a)
-        {
-            int issame = 0;
-            
-            for (int j:a)
+        int index = 0;
+        for( int i= 0;i<n;i++)
+        {  
+            if( (isdup(a[i],dup,n)) < 1)
             {
-                if(i == j)
-                {
-                    issame++;
-                }
+                dup[index] = a[i];
+                index++;
             }
+        }
+        
+        int[] r = new int[index];
+        for (int k = 0; k<index;k++)
+        {
+            r[k] = dup[k];
+        }
 
-            if(issame >1)
+        return r;
+    }
+
+    public static int isdup(int e,int[] a,int n)
+    {   
+        int r =0;
+        for(int i = 0; i<n;i++)
+        {
+            if( e == a[i])
             {
-
+                r++;
             }
         }
 
-        return ;
+        return r;
     }
+        public static void mDis(int[] a)
+        {   
+            System.out.println("Unique elements: ");
+
+                for(int i : a)
+                {
+                    System.out.print(i+" ");
+                }
+            System.out.println("");
+        }
+
 
      public static void main(String[] args) 
-    {
+    {   
         
-    
+
+         System.out.print("Enter the no of elements: ");   
+        n = sc.nextInt();
+        
+        mDis(DupR(mFill(n),n)); // filling the matrix and removing duplicates
+         // Display the matrix
     }
 }
